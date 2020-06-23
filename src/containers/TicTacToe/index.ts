da57e6ef941856e7861e93104188e10ad7625b09
Cristian Datu <datu.cristian@gmail.com>
2020-06-23 17:43:31 +0300

    Updated exports

--- src/containers/TicTacToe/index.ts
+++ src/containers/TicTacToe/index.ts
@@ -1,2 +1,4 @@
 export { default as TicTacToe } from "./TicTacToe";
+export { default as Board } from "./Board/Board";
+export { default as Square } from "./Board/Square/Square";
 export * from "./typedefs";
