704abfef8dbcf68bb8bfa3507756b9afe68a5ed0
Cristian Datu <datu.cristian@gmail.com>
2020-06-26 18:02:52 +0300

    Small UI enhancement for History page

--- src/components/ui/GameHistoryList/GameHistoryItem/GameHistoryItem.tsx
+++ src/components/ui/GameHistoryList/GameHistoryItem/GameHistoryItem.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { I_MatchData, Winners } from "../../../../containers/TicTacToe";
 
+import "./GameHistoryItem.scss";
+
 type GameHistoryItemProps = {
   matchNo: number;
   match: I_MatchData;
@@ -8,19 +10,29 @@ type GameHistoryItemProps = {
 
 function GameHistoryItem({ matchNo, match }: GameHistoryItemProps) {
   const { winner, history } = match;
-  const winnerText = winner === Winners.NO_WIN ? "No winner" : winner + " wins";
+  const winnerText = winner === Winners.NO_WIN ? "Draw" : winner;
   return (
-    <div>
-      <p>
-        Round {matchNo} - <strong>{winnerText}</strong> after {history.length}{" "}
-        moves
-      </p>
+    <article className="game-history-item">
+      <header className="game-history-item__header">
+        <h1 className="game-history-item__title">Round {matchNo}</h1>
+      </header>
+      <main className="game-history-item__main">
+        <dl className="game-history-item__stats">
+          <dt className="game-history-item__stats-label">Winner</dt>
+          <dd className="game-history-item__stats-value">{winnerText}</dd>
+          <dt className="game-history-item__stats-label">Moves</dt>
+          <dd className="game-history-item__stats-value">{history.length}</dd>
+        </dl>
+      </main>
+      <footer className="game-history-item__footer">
+        <button>Details</button>
+      </footer>
       {/* <Board
         winner={match.squares}
         squares={match.history[match.history.length - 1].board}
         onMove={() => {}}
       /> */}
-    </div>
+    </article>
   );
 }
 
